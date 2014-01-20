55c90d21aafb66bc5ef2b7fa238512b292c885b1
clement <maboiteaspam@wanadoo.fr>
2014-01-20 22:43:09 +0800

    Added new default capability on domain-switcher proto Added comments on domain switch proto

--- shared/js/vendors/utils/domain-switcher.js
+++ shared/js/vendors/utils/domain-switcher.js
@@ -4,22 +4,55 @@ define([],function () {
     /**
      * This script is responsible to provide the correct domain name to use
      * for webservices consumption according to the browsed domain name
+     *
      */
 
     var domain_switcher = function(){
         this.items = [];
         this.forced = false;
+        this.default = null;
     };
-    domain_switcher.prototype.reference = function(browsed, consumed,options){
+    /**
+     * Set the options for unreferenced domains
+     * @param consumed
+     * @param options
+     */
+    domain_switcher.prototype.default = function(consumed,options){
+        this.default = {
+            consumed:consumed,
+            options:options
+        };
+    };
+    /**
+     * Given browsed domain name,
+     * set its options specifcally
+     * @param browsed
+     * @param consumed
+     * @param options
+     */
+    domain_switcher.prototype.reference = function(browsed, consumed, options){
         this.items.push({
             browsed:browsed,
             consumed:consumed,
             options:options
         });
     };
+    /**
+     * get the string domain name to consume
+     * if currently_browsed is null,
+     * returns default options if exists
+     * otherwise,
+     * fallback to window.location.host default value
+     *
+     * @param currently_browsed|window.location.host
+     * @returns {*}
+     */
     domain_switcher.prototype.get_consumed = function(currently_browsed){
         if( this.forced ){
             return this.forced;
+        }
+        if(!currently_browsed && this.default){
+            return this.default.consumed;
         }
         currently_browsed = !currently_browsed?window.location.host:currently_browsed;
         for(var n in this.items ){
@@ -34,19 +67,40 @@ define([],function () {
                 }
             }
         }
-        console.log("ds : not found "+currently_browsed);
+        console.log("datasource : not found "+currently_browsed);
+        return false;
     };
-    domain_switcher.prototype.get = function(url){
+    /**
+     * Get a FQDN url to the consumed url in clear text
+     * @param url_append
+     * @returns {string}
+     */
+    domain_switcher.prototype.get = function(url_append){
         var domain = this.get_consumed();
-        return "http://"+domain.consumed+url;
+        return "http://"+domain.consumed+url_append;
     };
-    domain_switcher.prototype.getSsl = function(url){
+    /**
+     * Get a FQDN url to the consumed url in ssl
+     * if the referenced domain does not allow ssl, it will return a clear text version
+     * @param url_append
+     * @returns {string}
+     */
+    domain_switcher.prototype.getSsl = function(url_append){
         var domain = this.get_consumed();
-        return "http"+(domain.options.ssl?"s":"")+"://"+domain.consumed+url;
+        return "http"+(domain.options.ssl?"s":"")+"://"+domain.consumed+url_append;
     };
+    /**
+     * options for the currently browsed domain
+     * @returns {}
+     */
     domain_switcher.prototype.options = function(){
         return this.get_consumed().options;
     };
+    /**
+     * Explicitly force the currently browsed domain
+     *
+     * @param consumed
+     */
     domain_switcher.prototype.force = function(consumed){
         this.forced = consumed;
     };
